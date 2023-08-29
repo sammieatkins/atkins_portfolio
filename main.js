ffd172158c2d2f93bd0712ab4cd37e7c4196dbfc
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2023-08-29 14:56:34 -0500

    changing header back to normal, come back later

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ function makeHeader() {
     var header = document.getElementById("header");
 
     // Create elements to go into header
-// add a tag to h1 so it can also be the home page?
-    var h1 = document.createElement("h1");
+    var title = document.createElement("a");
     var home = document.createElement("a");
     var past_projects = document.createElement("a");
 
     // Fill header elements
-    h1.innerHTML = "Sammie Atkins";
+    title.innerHTML = "Sammie Atkins";
+    title.setAttribute("href", "index.html");
+    title.setAttribute("id", "sammie_atkins");
     home.innerHTML = "Home";
     home.setAttribute("href", "index.html");
     past_projects.innerHTML = "Past Projects";
     past_projects.setAttribute("href", "past_projects.html");
 
     // Insert into header
-    header.append(h1);
+    header.append(title);
     header.append(home);
     header.append(past_projects);
 }
-makeHeader();
